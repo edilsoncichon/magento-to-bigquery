4bbeb070e620ccd976e6eea3ac10dee04e79474b
Edilson Cichon <edilsoncichon@hotmail.com>
2019-07-01 17:59:19 -0300

    Orders Schema defined.

--- src/MagentoOrdersToBigQueryTransformer.php
+++ src/MagentoOrdersToBigQueryTransformer.php
@@ -2,14 +2,97 @@
 
 namespace ToBigQuery;
 
-class MagentoOrdersToBigQueryTransformer
+final class MagentoOrdersToBigQueryTransformer
 {
     public static function handle(array $item)
     {
-        return [
-            'entity_id' => $item['increment_id'],
-            'customer_name' => $item['customer_name'] ?? 'null',
-            'customer_email' => $item['customer_email'] ?? 'null',
-        ];
+        foreach ($item as $key => $value) {
+            if (is_array($value)) {
+                unset($item[$key]);
+            }
+        }
+
+        $formatted['entity_id'] = $item['entity_id'] ?? null;
+        $formatted['increment_id'] = $item['increment_id'] ?? null;
+        $formatted['is_virtual'] = isset($item['is_virtual']) ? !!$item['is_virtual'] : null;
+        $formatted['applied_rule_ids'] = $item['applied_rule_ids'] ?? null;
+        $formatted['base_currency_code'] = $item['base_currency_code'] ?? null;
+        $formatted['base_discount_amount'] = $item['base_discount_amount'] ?? null;
+        $formatted['base_discount_canceled'] = $item['base_discount_canceled'] ?? null;
+        $formatted['base_discount_invoiced'] = $item['base_discount_invoiced'] ?? null;
+        $formatted['base_discount_refunded'] = $item['base_discount_refunded'] ?? null;
+        $formatted['base_discount_tax_compensation_amount'] = $item['base_discount_tax_compensation_amount'] ?? null;
+        $formatted['base_discount_tax_compensation_invoiced'] = $item['base_discount_tax_compensation_invoiced'] ?? null;
+        $formatted['base_discount_tax_compensation_refunded'] = $item['base_discount_tax_compensation_refunded'] ?? null;
+        $formatted['base_grand_total'] = $item['base_grand_total'] ?? null;
+        $formatted['base_shipping_amount'] = $item['base_shipping_amount'] ?? null;
+        $formatted['base_shipping_incl_tax'] = $item['base_shipping_incl_tax'] ?? null;
+        $formatted['base_shipping_tax_amount'] = $item['base_shipping_tax_amount'] ?? null;
+        $formatted['base_subtotal'] = $item['base_subtotal'] ?? null;
+        $formatted['base_subtotal_incl_tax'] = $item['base_subtotal_incl_tax'] ?? null;
+        $formatted['base_tax_amount'] = $item['base_tax_amount'] ?? null;
+        $formatted['base_total_due'] = $item['base_total_due'] ?? null;
+        $formatted['base_to_global_rate'] = $item['base_to_global_rate'] ?? null;
+        $formatted['base_to_order_rate'] = $item['base_to_order_rate'] ?? null;
+        $formatted['billing_address_id'] = $item['billing_address_id'] ?? null;
+        $formatted['customer_id'] = $item['customer_id'] ?? null;
+        $formatted['customer_group_id'] = $item['customer_group_id'] ?? null;
+        $formatted['customer_email'] = $item['customer_email'] ?? null;
+        $formatted['customer_firstname'] = $item['customer_firstname'] ?? null;
+        $formatted['customer_is_guest'] = $item['customer_is_guest'] ? !!$item['customer_is_guest'] : null;
+        $formatted['discount_amount'] = $item['discount_amount'] ?? null;
+        $formatted['discount_canceled'] = $item['discount_canceled'] ?? null;
+        $formatted['discount_invoiced'] = $item['discount_invoiced'] ?? null;
+        $formatted['discount_refunded'] = $item['discount_refunded'] ?? null;
+        $formatted['discount_tax_compensation_amount'] = $item['discount_tax_compensation_amount'] ?? null;
+        $formatted['discount_tax_compensation_invoiced'] = $item['discount_tax_compensation_invoiced'] ?? null;
+        $formatted['discount_tax_compensation_refunded'] = $item['discount_tax_compensation_refunded'] ?? null;
+        $formatted['email_sent'] = $item['email_sent'] ? !!$item['email_sent'] : null;
+        $formatted['global_currency_code'] = $item['global_currency_code'] ?? null;
+        $formatted['grand_total'] = $item['grand_total'] ?? null;
+        $formatted['order_currency_code'] = $item['order_currency_code'] ?? null;
+        $formatted['protect_code'] = $item['protect_code'] ?? null;
+        $formatted['quote_id'] = $item['quote_id'] ?? null;
+        $formatted['remote_ip'] = $item['remote_ip'] ?? null;
+        $formatted['shipping_amount'] = $item['shipping_amount'] ?? null;
+        $formatted['shipping_canceled'] = $item['shipping_canceled'] ?? null;
+        $formatted['shipping_description'] = $item['shipping_description'] ?? null;
+        $formatted['shipping_discount_amount'] = $item['shipping_discount_amount'] ?? null;
+        $formatted['shipping_discount_tax_compensation_amount'] = $item['shipping_discount_tax_compensation_amount'] ?? null;
+        $formatted['shipping_incl_tax'] = $item['shipping_incl_tax'] ?? null;
+        $formatted['shipping_invoiced'] = $item['shipping_invoiced'] ?? null;
+        $formatted['shipping_refunded'] = $item['shipping_refunded'] ?? null;
+        $formatted['shipping_tax_amount'] = $item['shipping_tax_amount'] ?? null;
+        $formatted['shipping_tax_refunded'] = $item['shipping_tax_refunded'] ?? null;
+        $formatted['state'] = $item['state'] ?? null;
+        $formatted['status'] = $item['status'] ?? null;
+        $formatted['store_currency_code'] = $item['store_currency_code'] ?? null;
+        $formatted['store_id'] = $item['store_id'] ?? null;
+        $formatted['store_name'] = $item['store_name'] ?? null;
+        $formatted['store_to_base_rate'] = $item['store_to_base_rate'] ?? null;
+        $formatted['store_to_order_rate'] = $item['store_to_order_rate'] ?? null;
+        $formatted['subtotal'] = $item['subtotal'] ?? null;
+        $formatted['subtotal_canceled'] = $item['subtotal_canceled'] ?? null;
+        $formatted['subtotal_incl_tax'] = $item['subtotal_incl_tax'] ?? null;
+        $formatted['subtotal_invoiced'] = $item['subtotal_invoiced'] ?? null;
+        $formatted['subtotal_refunded'] = $item['subtotal_refunded'] ?? null;
+        $formatted['tax_amount'] = $item['tax_amount'] ?? null;
+        $formatted['tax_canceled'] = $item['tax_canceled'] ?? null;
+        $formatted['tax_invoiced'] = $item['tax_invoiced'] ?? null;
+        $formatted['tax_refunded'] = $item['tax_refunded'] ?? null;
+        $formatted['total_canceled'] = $item['total_canceled'] ?? null;
+        $formatted['total_due'] = $item['total_due'] ?? null;
+        $formatted['total_invoiced'] = $item['total_invoiced'] ?? null;
+        $formatted['total_item_count'] = $item['total_item_count'] ?? null;
+        $formatted['total_offline_refunded'] = $item['total_offline_refunded'] ?? null;
+        $formatted['total_online_refunded'] = $item['total_online_refunded'] ?? null;
+        $formatted['total_paid'] = $item['total_paid'] ?? null;
+        $formatted['total_qty_ordered'] = $item['total_qty_ordered'] ?? null;
+        $formatted['total_refunded'] = $item['total_refunded'] ?? null;
+        $formatted['weight'] = $item['weight'] ?? null;
+        $formatted['created_at'] = $item['created_at'] ?? null;
+        $formatted['updated_at'] = $item['updated_at'] ?? null;
+
+        return $formatted;
     }
 }
--- src/PushMagentoOrdersToBigQuery.php
+++ src/PushMagentoOrdersToBigQuery.php
@@ -59,7 +59,7 @@ final class PushMagentoOrdersToBigQuery
         $errors = '';
         foreach ($insertResponse->failedRows() as $row) {
             foreach ($row['errors'] as $error) {
-                $errors .= sprintf('%s: %s' . PHP_EOL, $error['reason'], $error['message']);
+                $errors .= sprintf('[%s] %s: %s' . PHP_EOL, $error['location'], $error['reason'], $error['message']);
             }
         }
         throw new Exception($errors);
@@ -77,10 +77,166 @@ final class PushMagentoOrdersToBigQuery
                 'metadata' => [
                     'schema' => [
                         'fields' => [
-                            //TODO ESPEFICICAR SCHEMA!
-                            ['name' => 'entity_id', 'type' => 'integer'],
-                            ['name' => 'customer_name', 'type' => 'string'],
+
+                            ['name' => 'entity_id', 'type' => 'string'],
+
+                            ['name' => 'increment_id', 'type' => 'string'],
+
+                            ['name' => 'is_virtual', 'type' => 'boolean'],
+
+                            ['name' => 'applied_rule_ids', 'type' => 'string'],
+
+                            ['name' => 'base_currency_code', 'type' => 'string'],
+
+                            ['name' => 'base_discount_amount', 'type' => 'float'],
+
+                            ['name' => 'base_discount_canceled', 'type' => 'float'],
+
+                            ['name' => 'base_discount_invoiced', 'type' => 'float'],
+
+                            ['name' => 'base_discount_refunded', 'type' => 'float'],
+
+                            ['name' => 'base_discount_tax_compensation_amount', 'type' => 'float'],
+
+                            ['name' => 'base_discount_tax_compensation_invoiced', 'type' => 'float'],
+
+                            ['name' => 'base_discount_tax_compensation_refunded', 'type' => 'float'],
+
+                            ['name' => 'base_grand_total', 'type' => 'float'],
+
+                            ['name' => 'base_shipping_amount', 'type' => 'float'],
+
+                            ['name' => 'base_shipping_incl_tax', 'type' => 'float'],
+
+                            ['name' => 'base_shipping_tax_amount', 'type' => 'float'],
+
+                            ['name' => 'base_subtotal', 'type' => 'float'],
+
+                            ['name' => 'base_subtotal_incl_tax', 'type' => 'float'],
+
+                            ['name' => 'base_tax_amount', 'type' => 'float'],
+
+                            ['name' => 'base_total_due', 'type' => 'float'],
+
+                            ['name' => 'base_to_global_rate', 'type' => 'float'],
+
+                            ['name' => 'base_to_order_rate', 'type' => 'float'],
+
+                            ['name' => 'billing_address_id', 'type' => 'integer'],
+
+                            ['name' => 'customer_id', 'type' => 'integer'],
+
+                            ['name' => 'customer_group_id', 'type' => 'integer'],
+
                             ['name' => 'customer_email', 'type' => 'string'],
+
+                            ['name' => 'customer_firstname', 'type' => 'string'],
+
+                            ['name' => 'customer_is_guest', 'type' => 'boolean'],
+
+                            ['name' => 'discount_amount', 'type' => 'float'],
+
+                            ['name' => 'discount_canceled', 'type' => 'float'],
+
+                            ['name' => 'discount_invoiced', 'type' => 'float'],
+
+                            ['name' => 'discount_refunded', 'type' => 'float'],
+
+                            ['name' => 'discount_tax_compensation_amount', 'type' => 'float'],
+
+                            ['name' => 'discount_tax_compensation_invoiced', 'type' => 'float'],
+
+                            ['name' => 'discount_tax_compensation_refunded', 'type' => 'float'],
+
+                            ['name' => 'email_sent', 'type' => 'boolean'],
+
+                            ['name' => 'global_currency_code', 'type' => 'string'],
+
+                            ['name' => 'grand_total', 'type' => 'float'],
+
+                            ['name' => 'order_currency_code', 'type' => 'string'],
+
+                            ['name' => 'protect_code', 'type' => 'string'],
+
+                            ['name' => 'quote_id', 'type' => 'integer'],
+
+                            ['name' => 'remote_ip', 'type' => 'string'],
+
+                            ['name' => 'shipping_amount', 'type' => 'float'],
+
+                            ['name' => 'shipping_canceled', 'type' => 'float'],
+
+                            ['name' => 'shipping_description', 'type' => 'string'],
+
+                            ['name' => 'shipping_discount_amount', 'type' => 'float'],
+
+                            ['name' => 'shipping_discount_tax_compensation_amount', 'type' => 'float'],
+
+                            ['name' => 'shipping_incl_tax', 'type' => 'float'],
+
+                            ['name' => 'shipping_invoiced', 'type' => 'float'],
+
+                            ['name' => 'shipping_refunded', 'type' => 'float'],
+
+                            ['name' => 'shipping_tax_amount', 'type' => 'float'],
+
+                            ['name' => 'shipping_tax_refunded', 'type' => 'float'],
+
+                            ['name' => 'state', 'type' => 'string'],
+
+                            ['name' => 'status', 'type' => 'string'],
+
+                            ['name' => 'store_currency_code', 'type' => 'string'],
+
+                            ['name' => 'store_id', 'type' => 'integer'],
+
+                            ['name' => 'store_name', 'type' => 'string'],
+
+                            ['name' => 'store_to_base_rate', 'type' => 'float'],
+
+                            ['name' => 'store_to_order_rate', 'type' => 'float'],
+
+                            ['name' => 'subtotal', 'type' => 'float'],
+
+                            ['name' => 'subtotal_canceled', 'type' => 'float'],
+
+                            ['name' => 'subtotal_incl_tax', 'type' => 'float'],
+
+                            ['name' => 'subtotal_invoiced', 'type' => 'float'],
+
+                            ['name' => 'subtotal_refunded', 'type' => 'float'],
+
+                            ['name' => 'tax_amount', 'type' => 'float'],
+
+                            ['name' => 'tax_canceled', 'type' => 'float'],
+
+                            ['name' => 'tax_invoiced', 'type' => 'float'],
+
+                            ['name' => 'tax_refunded', 'type' => 'float'],
+
+                            ['name' => 'total_canceled', 'type' => 'float'],
+
+                            ['name' => 'total_due', 'type' => 'float'],
+
+                            ['name' => 'total_invoiced', 'type' => 'float'],
+
+                            ['name' => 'total_item_count', 'type' => 'integer'],
+
+                            ['name' => 'total_offline_refunded', 'type' => 'float'],
+
+                            ['name' => 'total_online_refunded', 'type' => 'float'],
+
+                            ['name' => 'total_paid', 'type' => 'float'],
+
+                            ['name' => 'total_qty_ordered', 'type' => 'integer'],
+
+                            ['name' => 'total_refunded', 'type' => 'float'],
+
+                            ['name' => 'weight', 'type' => 'integer'],
+
+                            ['name' => 'created_at', 'type' => 'datetime'],
+
+                            ['name' => 'updated_at', 'type' => 'datetime'],
                         ]
                     ]
                 ]
